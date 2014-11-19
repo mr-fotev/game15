9ac7367e89bed4e1f2af63fb2da5e8166e43accf
mr-fotev <itsofaraona@abv.bg>
2014-11-19 03:18:22 +0200

    Update logic.clj

--- src/game15/logic.clj
+++ src/game15/logic.clj
@@ -11,22 +11,19 @@
   (let [n (sq s), v (shuffle (range 1 n))]
     (if (even? (count-inversions v (dec n)))
       (conj v 0)
-      (conj (vec (conj (drop 2 v)
-                       (first v)
-                       (second v)))
-            0))))
+      (conj (vec (conj (drop 2 v) (first v) (second v))) 0))))
 
 (defn move [g n]
   "Moves game g on index n."
   (let [s (:size g), brd (:board g), i (quot n s), j (rem n s)]
     (letfn [(pos? [i1 j1 x] (and (< -1 i1 s) (< -1 j1 s) (= (:pos g) x)))
-            (res [x] {:size s, :pos n, :board (assoc brd x (brd n), n 0), :moves (inc (:moves g))})]
-      (cond
-       (pos? i (inc j) (inc n)) (res (inc n))
-       (pos? (dec i) j (- n s)) (res (- n s))
-       (pos? i (dec j) (dec n)) (res (dec n))
-       (pos? (inc i) j (+ n s)) (res (+ n s))
-       :else g))))
+      (res [x] {:size s, :pos n, :board (assoc brd x (brd n), n 0), :moves (inc (:moves g))})]
+        (cond
+          (pos? i (inc j) (inc n)) (res (inc n))
+          (pos? (dec i) j (- n s)) (res (- n s))
+          (pos? i (dec j) (dec n)) (res (dec n))
+          (pos? (inc i) j (+ n s)) (res (+ n s))
+          :else g))))
 
 (defn win? [g]
   "Checks if the player has solved the board."
